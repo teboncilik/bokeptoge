7ee82e50c75d8f33e16d53e5f46e4634aeb044f9
teboncilik <161819767+teboncilik@users.noreply.github.com>
2024-07-27 03:20:40 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,13 +19,14 @@ export default async function Home({
     const query = (searchParams.q && (searchParams.q as string)) || undefined;
 
     return (
-        <div className="md:my-2"><h1>{SITENAME}</h1><h2>Video Bokep Indo Jepang Barat Terbaru</h2>
+        <div className="md:my-2">
          <Script src="https://js.juicyads.com/jp.php?c=947403z2v256s2x2w264x294&u=http%3A%2F%2Fwww.juicyads.rocks" />
             {query ? (
                 <SearchCardList query={query} banner />
             ) : (
-                <SearchCardList query={SITENAME.split(" ")[1]} />
+                <CardList page={page} per_page={per_page} fld_id={fld_id} />
             )}
+<h1>{SITENAME}</h1><h2>Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru</h2><p>Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pijat pelajar Hijab Abg Colmek Film Tante Hot Twitter Asia Download Live stw situs bokep indonesia jepang barat korea japan jav cina japanese china rusia arab india thailand nonton link sd crot playbokep simontok bokepin montok baru perawan anak kecil telegram selingkuh ojol cantik gay vidio lokal artis pelajar janda streaming hd anime hentai bokepind gudang avtub pijat sotwe rumah pemerkosaan inggris xpanas pure tobrut vcs ngintip binor remaja yandex update perselingkuhan wiki raja full com porno indoh</p>
         </div>
     );
 }
